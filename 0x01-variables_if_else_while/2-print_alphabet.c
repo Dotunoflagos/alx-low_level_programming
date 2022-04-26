e1699b3445ed1ed09e76cf3d22b6fdfc23b1c616
Adedotun balogun <adedotun_balogun_yahoo.com>
2022-04-26 14:57:59 -0700

    test2upd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,8 @@
 int main(void)
 {
 	char low;
-
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
-
+		printf("\n");
 	return (0);
 }
