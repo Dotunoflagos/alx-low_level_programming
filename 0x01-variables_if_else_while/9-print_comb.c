970f00b53d8fd90f157f62f2d4268d72b7307ae4
Adedotun balogun <adedotun_balogun_yahoo.com>
2022-04-26 18:22:19 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,5 @@
 /*
  * File: 9-print_comb.c
- *
  * Auth: Brennan D Baraban
  */
 
@@ -20,7 +19,7 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-			
+
 		putchar(',');
 		putchar(' ');
 	}
