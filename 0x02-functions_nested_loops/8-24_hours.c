7f30838890d80371cab1c750026392789d9290f6
adedotun <adedotun_balogun@yahoo.com>
2022-07-06 16:05:03 -0700

    8-24_hours.c.v2.1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,13 @@ void jack_bauer(void)
 {
 	int c, d, e, f;
 
-	for (c = 48; c < 50;  c++)
+	for (c = 48; c < 51;  c++)
 	{
-		for (d = 48; d < 57; d++)
+		for (d = 48; d < 58; d++)
 		{
-			for (e = 48; e < 53; e++)
+			for (e = 48; e < 54; e++)
 			{
-				for (f = 48; f < 57; f++)
+				for (f = 48; f < 58; f++)
 				{
 				_putchar(c);
 				_putchar(d);
