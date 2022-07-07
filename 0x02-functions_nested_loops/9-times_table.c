6e2b554a2591cdb751cd7c70ec00a9a26006cf77
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 13:26:59 -0700

    9-times_table.c.v4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,53 @@
 #include "main.h"
 
 /**
-* void times_table - Prints times table.
-* print - prints single charecter with _printchar
+* print - prints single charecter with _printchar.
+* @n: number to be printed.
 */
 void print(long n)
 {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    if (n/10)
-        print(n/10);
+	if (n / 10)
+		print(n / 10);
 
-    _putchar(n%10 + '0');
+	_putchar(n % 10 + '0');
 }
 
+/**
+* times_table - Prints times table.
+*/
 void times_table(void)
 {
-        int c, d, e;
+	int c, d, e;
 
-        for (c = 0; c <= 9;  c++)
-        {
-                for (d = 0; d <= 9; d++)
-                {
-                        e = c * d;
+	for (c = 0; c <= 9;  c++)
+	{
+		for (d = 0; d <= 9; d++)
+		{
+			e = c * d;
 
-                        if (d != 0)
-                        {
-                                if (e > 9)
-                                {
-                                        _putchar(' ');
-                                }
-                                else
-                                {
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                }
-                        }
-                        print(e);
-                        if (d == 9)
-                                _putchar('\n');
-                        else
-                                _putchar(',');
-                }
-        }
+			if (d != 0)
+			{
+				if (e > 9)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			print(e);
+			if (d == 9)
+				_putchar('\n');
+			else
+				_putchar(',');
+		}
+	}
 }
