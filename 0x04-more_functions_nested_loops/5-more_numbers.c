e91200b20e4227f86e51b62bd654025e62303278
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 19:05:22 -0700

    ..v5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print - prints the alphabet in lowercase.
+ * @n: to be printed
+ */
+void print(int n)
+{
+	if (n / 10)
+		print(n / 10);
+
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * print_most_numbers - prints the alphabet in lowercase.
+ */
+void print_most_numbers(void)
+{
+	int c, d;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (d = 0; d <= 14; d++)
+			print(d);
+
+		_putchar('\n');
+	}
+}
