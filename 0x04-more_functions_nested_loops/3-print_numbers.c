2b39a68c5d9f396fdddec99bd8d6265cc25b2638
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 18:14:53 -0700

    ..v8

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_numbers - prints the alphabet in lowercase, followed by a new line.
+*/
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c + '0');
+}
