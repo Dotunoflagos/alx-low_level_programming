1979cec736f21404dda21dd6b62a065fcfcc4766
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 18:03:49 -0700

    ..v5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - prints the alphabet in lowercase, followed by a new line.
+* @a: super story
+* @b: super 2
+* Return: 1 0r 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
