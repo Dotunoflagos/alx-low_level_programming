83b68657c640a9027d393f7cc6d1f0b9f75ef850
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 21:30:04 -0700

    ..9v1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 3 == 0 && c % 5 == 0)
+		{
+			printf("%s%s", f, b);
+			ptchar(' ');
+		}
+		else if (c % 3 == 0)
+		{
+			printf("%s", f);
+			putchar(' ');
+		}
+		else if (c % 5 == 0)
+		{
+			printf("%s", b);
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", c);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
