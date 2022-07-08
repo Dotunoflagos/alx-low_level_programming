b1c914b22ccb37d378f3dbcbdf9bd59eba7b0a2c
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 17:59:54 -0700

    ..v4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - prints the alphabet in lowercase, followed by a new line.
+* @c: super story
+* Return: 1 0r 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
