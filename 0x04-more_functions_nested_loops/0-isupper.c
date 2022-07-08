f9f3535178476dbaaf7e9d83218c5f1153bcfcd1
adedotun <adedotun_balogun@yahoo.com>
2022-07-07 17:31:51 -0700

    0-isupper.c.v1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - prints the alphabet in lowercase, followed by a new line
+* @c : super story
+* return : 1 0r 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
