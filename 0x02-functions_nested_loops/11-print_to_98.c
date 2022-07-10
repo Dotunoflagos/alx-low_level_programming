f354324e737eb06d69c0f0d3fbab8c069a39e3a4
adedotun <adedotun_balogun@yahoo.com>
2022-07-09 18:23:59 -0700

    11-print_to_98.c.v4

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (c = n; c != 98; c--)
+		for (c = n; c >= 98; c--)
 		{
 			printf("%d", c);
-			if (c < 98)
+			if (c > 98)
 				printf(", ");
 			else
 				putchar('\n');
