ae373dc4a144b0fe0580888e5a1cebfeecd8379c
adedotun <adedotun_balogun@yahoo.com>
2022-07-13 17:48:24 -0700

    all

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - prints the alphabet in lowercase, followed by a new line.
+* @n: super story
+* Return: 1 0r 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Balogun adedotun
+ * Desc: Header file containing declarations for all functions
+ */
+
+void reset_to_98(int *n);
+#endif
