4f949365bd21515f5913775630da4db386ee59ca
adedotun <adedotun_balogun@yahoo.com>
2022-07-15 01:38:27 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - prints the alphabet in lowercase, followed by a new line.
+* @str: super story
+* Return: count
+*/
+
+void _puts(char *str)
+{
+	char *c = str;
+	while (*c != '\0')
+	{
+		_putchar(str + c);
+		c++;
+	}
+}
