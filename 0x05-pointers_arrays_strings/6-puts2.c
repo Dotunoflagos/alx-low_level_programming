09f0658215e3b7b93457337e279a75514da4e749
adedotun <adedotun_balogun@yahoo.com>
2022-07-17 16:09:37 -0700

    even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* rev_string - prints the alphabet in lowercase, followed by a new line.
+* @s: super story
+* Return: count
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int len = 0;
+	char c;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		c = str[i];
+		if (c % 2 == 0)
+			_printf(c);
+	}
+
+}
