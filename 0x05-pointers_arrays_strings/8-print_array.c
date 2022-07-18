37d2a62ebed325b0ad5e7b6517a5a5c810d014c6
adedotun <adedotun_balogun@yahoo.com>
2022-07-18 05:12:32 -0700

    print ARR

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,16 +8,15 @@
 
 void print_array(int *a, int n)
 {
-	int i, len;
-	
-	len = 0;
-	while (a[len] != '\0')
-		len++;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
+		
 		if (i != n - 1)
 			printf(", ");
-	}	
+	}
+
+	putchar('\n');
 }
