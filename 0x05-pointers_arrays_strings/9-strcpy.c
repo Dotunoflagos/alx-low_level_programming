226919b00676dc1595c1c4909427abc622c0dab7
adedotun <adedotun_balogun@yahoo.com>
2022-07-18 07:57:31 -0700

    dfsdsf

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,15 +12,10 @@ char *_strcpy(char *dest, char *src)
 	int i;
 	int len = 0;
 	
-	if (src[1] == '\n')
-		dest[0] = src[0];
-	else
-	{
-		while (src[len] != '\n')
-			len++;
-
-		for (i = 0; i <= len; i++)
-			dest[i] = src[i];
+	while (src[len])
+		len++;
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
 	}
 	return(dest);
 }
