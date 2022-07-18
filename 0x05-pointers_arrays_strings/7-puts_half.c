a3f1ba117cd697bd344f59d4bf00a1e1635974b9
adedotun <adedotun_balogun@yahoo.com>
2022-07-17 17:08:37 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* puts2 - prints the alphabet in lowercase, followed by a new line.
+* @str: super story
+* Return: count
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+	char c;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len / 2; i < len; i++)
+	{
+		c = str[i];
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
