98a33bb03fea216bce42144bd2b55e529496c9e1
adedotun <adedotun_balogun@yahoo.com>
2022-07-18 04:29:40 -0700

    print arr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_array - prints the alphabet in lowercase, followed by a new line.
+* @a: super story
+* @n: count
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		printf("%d, ", a[i]);
+
+}
