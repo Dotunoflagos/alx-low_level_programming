e3b17e460e02095506b8992362695c75b3052fd4
adedotun <adedotun_balogun@yahoo.com>
2022-07-19 02:53:03 -0700

    concatinate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strcat - concactinates
+  * @dest: destination
+  * @src: source
+  * Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0, srclen = 0, i = 0;
+
+	while (dest[destlen])
+		destlen++;
+
+	while (dist[srclen])
+		srclen++;
+
+	for (i = destlen; i <= srclen; i++)
+		dest[i] = src[i - destlen];
+
+	return (dest);
+}
