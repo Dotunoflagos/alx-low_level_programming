2818d3a6bdbecd7641c863cedac6ab16771c3a9b
adedotun <adedotun_balogun@yahoo.com>
2022-07-20 10:10:56 -0700

    hope it workes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _strncpy - copies a string
+  * @dest: destination
+  * @src: source
+  * @n: number
+  * Return:dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int len = 0;
+
+	while (src[len])
+		len++;
+
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+
+	if (len < n)
+	{
+		for ( ; i <= n - len; i++)
+		{
+			dist[i] = '\0';
+		}
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strcpy - prints the alphabet in lowercase, followed by a new line.
+* @dest: super story
+* @src: count
+* Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (src[len])
+		len++;
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
