ec2f08f1d4f206f8d6681ef1687c647447609263
adedotun <adedotun_balogun@yahoo.com>
2022-07-20 16:28:35 -0700

    message

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,19 +10,29 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 0, b = n;
 	int len = 0;
+
+	while (src[len])
+		len++;
 
 	if (src[0] != '\0')
 	{
-		while (src[len])
-			len++;
+		if (n > len)
+			n = len;
+
 		for ( ; i < n; i++)
 			dest[i] = src[i];
 	}
+
 	if (len < n)
 	{
 		for ( ; i <= n + len - 1; i++)
+			dest[i] = '\0';
+	}
+	else
+	{
+		for ( ; i <= b - 1; i++)
 			dest[i] = '\0';
 	}
 	return (dest);
