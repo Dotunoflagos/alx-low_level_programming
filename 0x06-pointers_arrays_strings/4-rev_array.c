663883e3774b0b29fc35d3e600bd4938807602a8
adedotun <adedotun_balogun@yahoo.com>
2022-07-21 07:24:40 -0700

    mess

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,8 +13,9 @@ void reverse_array(int *a, int n)
 	for (i = 0 ; i < n; i++)
 	{
 		int tmp = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = tmp;
+		
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = tmp;
 		
 	}
 }
