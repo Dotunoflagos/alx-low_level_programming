5f907122c22318ffc8bed01cef69c70ddf14e370
adedotun <adedotun_balogun@yahoo.com>
2022-07-21 05:28:18 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * reverse_array - reverse array
+  * @a: array
+  * @n: number
+  */
+
+void reverse_array(int *a, int n)
+{
+	int b[], i = 0;
+
+	while (*a != '\0')
+	{
+		b[i] = *a;
+		i++;
+		a++;
+	}
+	for (i = n ; i <= 0; i--)
+	{
+		a[i] = b[n - i];
+	}
+}
