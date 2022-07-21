001db66a0cac04b7313eef1f86c7159c650ac331
adedotun <adedotun_balogun@yahoo.com>
2022-07-21 07:33:53 -0700

    yes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	
-	for (i = 0 ; i < n/2; i++)
+
+	for (i = 0 ; i < n / 2; i++)
 	{
 		int tmp = a[i];
-		
+
 		a[i] = a[(n - 1) - i];
 		a[(n - 1) - i] = tmp;
-		
 	}
 }
