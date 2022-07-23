5d0a3f2b5b53dcf9bfacf56bdd8f7a22fe68b4d7
adedotun <adedotun_balogun@yahoo.com>
2022-07-23 15:09:03 -0700

    yes3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,13 @@ char *cap_string(char *ch)
 
 	for ( ; i <= chlen; i++)
 	{
-		if (i == 0 && (ch[i] <= 65 || ch[i] >= 90))
+		if (i == 0 && (ch[i] >= 65 && ch[i] <= 122))
 		{
 			ch[i] -= 32;
 		}
 		else if (ch[i] >= 97 && ch[i] <= 122)
 		{
-			if ((ch[i - 1] < 97 || ch[i - 1] > 122) && ch[i - 1] != 45)
+			if ((ch[i - 1] < 97 || ch[i - 1] > 122) && !(ch[i - 1] >= 65 && ch[i - 1] <= 90) && ch[i - 1] != 45)
 				ch[i] -= 32;
 		}
 	}
