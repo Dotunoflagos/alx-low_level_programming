e940ca5214ee3972cb6df5f4d2225de6f4ef9c42
adedotun <adedotun_balogun@yahoo.com>
2022-07-23 14:15:58 -0700

    message

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalise wwords
+ * @ch: input character
+ */
+
+char *cap_string(char *ch)
+{
+	int chLen = 0, i = 0;
+
+	while (ch[chlen] != '\n')
+		chlen++;
+
+	for ( ; i <= chLen; i++)
+	{
+		if (i == 0)
+		{
+			ch[i] += 32;
+		}
+		else if (ch[i] >= 79 && ch[i] <= 122)
+		{
+			if ((ch[i - 1] < 79 && ch[i - 1] > 122) && ch[i - 1] != 45)
+				ch[i] += 32;
+		}
+	}
+}
