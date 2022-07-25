a355afc195d0b9569ff736927fff50b4bb5db7d5
adedotun <adedotun_balogun@yahoo.com>
2022-07-25 03:08:26 -0700

    search for character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - search for character
+ * @c: character to be searched for
+ * @s: string to be searched
+ * Return: pointer to character or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] = c)
+			return (s[i]);
+		else
+			return (NULL);
+		i++;
+	}
+}
