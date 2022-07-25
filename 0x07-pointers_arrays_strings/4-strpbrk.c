2332f6a104836835f586fa465a324d7d9530d02c
adedotun <adedotun_balogun@yahoo.com>
2022-07-25 06:46:39 -0700

    m

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search for charactyers
+ * @s: string
+ * @accept: to be searched for
+ * Return: number
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int slen = 0, acclen = 0, i = 0, j = 0;
+
+	while (s[slen] != '\0')
+		slen++;
+
+	while (accept[acclen] != '\0')
+		acclen++;
+
+	for (i = 0; i <= slen - 1; i++)
+	{
+		for (j = 0; j <= acclen - 1; j++)
+		{
+			if (s[i] != accept[j])
+			{
+				s[i] = '\0';
+			}
+		}
+	}
+
+	return (tmp);
+}
