f331b4cf84fb12d3608500799114de9b75fee750
adedotun <adedotun_balogun@yahoo.com>
2022-07-24 19:26:05 -0700

    Fills memory with constant bytes

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Balogun adedotun
+ * Desc: Header file containing declarations for all functions
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+#endif
