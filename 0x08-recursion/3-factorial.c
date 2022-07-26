c6ce4936dfb297f5ed4c8e5bb39a122f1a13e738
adedotun <adedotun_balogun@yahoo.com>
2022-07-26 06:15:38 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - prints strings
+ * @n: to be printed
+ * Return: breaks loop
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else (n < 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
