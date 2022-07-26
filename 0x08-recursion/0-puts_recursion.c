1dd26fc59a9557b4c30f02c7402bbabf7791f424
adedotun <adedotun_balogun@yahoo.com>
2022-07-26 03:57:53 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recusion - prints strings
+ * @s: to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Balogun adedotun
+ * Desc: Header file containing declarations for all functions
+ */
+
+void _puts_recursion(char *s);
+#endif
