920b832106e13a65d28211067d6f44f65ee321b7
adedotun <adedotun_balogun@yahoo.com>
2022-07-26 04:54:50 -0700

    reverse printing with recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints strings
+ * _puts_rev_recursion - prints strings
  * @s: to be printed
  * Return: breaks loop
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	int i;
-
 	if (*s != '\0')
 	{
-		i++;
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	if (i - 1 != 0)
-		_putchar(s[i]);
-	else
-		_putchar('\0');
 }
