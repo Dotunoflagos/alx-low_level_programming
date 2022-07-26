218b09dbe2927752b5fdc947aae398a0c0cc4804
adedotun <adedotun_balogun@yahoo.com>
2022-07-26 16:49:00 -0700

    creats array

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Balogun adedotun
+ * Desc: Header file containing declarations for all functions
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif
