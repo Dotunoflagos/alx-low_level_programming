332817f9bd3c19869db6373c1a27acd18f6a00d3
adedotun <adedotun_balogun@yahoo.com>
2022-07-26 23:25:15 -0700

    creats array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,18 +12,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	int i;
 
 	if (size == 0)
 		return ('\0');
 
 	arr = malloc(sizeof(char) *  size);
-	if (*arr == '\0')
+	if (arr == '\0')
 		return ('\0');
 
-	while (size > 0)
+	while (i < size)
 	{
-		arr[size] = c;
-		size--;
+		arr[i] = c;
+		i++;
 	}
 	return (arr);
 }
