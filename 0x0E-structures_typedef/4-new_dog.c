dd4cff40a5aa679d75e5d1bc7c40ab3c1e1ed760
adedotun <adedotun_balogun@yahoo.com>
2022-08-04 03:50:25 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,10 +19,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-    for (new_name = 0 ; name[new_name] == '\0'; new_name++)
-        ;
-    for (new_owner = 0 ; owner[new_owner] == '\0'; new_owner++)
-        ;
+	for (new_name = 0 ; name[new_name] == '\0'; new_name++)
+		;
+	for (new_owner = 0 ; owner[new_owner] == '\0'; new_owner++)
+		;
 
 	new_dog->name = malloc(new_name + 1);
 	new_dog->owner = malloc(new_owner + 1);
@@ -37,8 +37,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	for (i = 0 ; i < new_name ; i++)
 		new_dog->name[i] = name[i];
-        
-    new_dog->name[i] = '\0';
+
+	new_dog->name[i] = '\0';
 	new_dog->age = age;
 
 	for (i = 0 ; i < new_owner ; i++)
