d2927a9e667f5d353020bc9492b811db6bd9c784
adedotun <adedotun_balogun@yahoo.com>
2022-08-03 18:36:53 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef pet
+#define pet
+
+/**
+ * struct dog - define a new type of struct
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
